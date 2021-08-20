36fe2dda04750761665f42ba5061a0ebf2581d46
Amanda Morgan <AmandaC0022@gmail.com>
2021-08-20 11:17:11 -0500

    license link now displays

---
+++ userREADME.md
@@ -0,0 +1,37 @@
+# t
+
+
+
+## Description: 
+t
+
+  ## Table of Contents 
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [Credits](#credits)
+  4. [License](#license)
+  5. [Contribution](#contribution)
+  6. [Tests](#tests)
+  7. [Questions](#questions)
+
+  ## Installation <a name="installation"></a>  
+  t 
+
+  ## Usage  
+  t
+
+  ## Credits 
+  t
+
+  ## License 
+  https://opensource.org/licenses/MIT
+
+  ## Contribution
+  t
+  
+  ## Tests 
+  t
+  
+  ## Questions 
+  t 
+  Please contact me using my GitHub profile: t
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,11 @@
 // // returns a license badge based on which license is passed in
-// function renderLicenseBadge(license) {
-//   if(license) {
-//     return `![License](https://img.shields.io/badge/License-${license}-yellow.svg)`
-//   } else {
-//     return " "
-//   }
-// }
+function renderLicenseBadge(license) {
+  if(license) {
+    return `![License](https://img.shields.io/badge/License-${license}-yellow.svg)`
+  } else {
+    return " "
+  }
+}
 
 // var license = data.license 
 // if (license === 'MIT') {
@@ -21,20 +21,19 @@
 
 
 // returns the license link
-// function renderLicenseLink(license) {
-// var license = data.license 
-// if (license === 'MIT') {
-//   return `https://opensource.org/licenses/MIT`; 
-// }
-// if (license === 'BSD') {
-//   return `https://opensource.org/licenses/BSD-2-Clause`; 
-// }
-// if (license === 'GLP') {
-//   return `https://www.gnu.org/licenses/gpl-3.0`; 
-// } 
-// // If there is no license, return an empty string
-// return ' '; 
-// }
+function renderLicenseLink(license) { 
+if (license === 'MIT') {
+  return `https://opensource.org/licenses/MIT`; 
+}
+if (license === 'BSD') {
+  return `https://opensource.org/licenses/BSD-2-Clause`; 
+}
+if (license === 'GLP') {
+  return `https://www.gnu.org/licenses/gpl-3.0`; 
+} 
+// If there is no license, return an empty string
+return ' '; 
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -71,7 +70,7 @@ ${data.description}
   ${data.credits}
 
   ## License 
-
+  ${renderLicenseLink(data.license)}
 
   ## Contribution
   ${data.contribution}
