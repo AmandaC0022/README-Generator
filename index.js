02dec473a406ded9238e6e25020ba9f0d883f500
Amanda Morgan <AmandaC0022@gmail.com>
2021-08-20 08:31:12 -0500

    added questions to prompt

--- index.js
+++ index.js
@@ -5,10 +5,61 @@ const markdownTemplate = require('./utils/generateMarkdown.js');
 inquirer
   .prompt([
     /* Pass your questions in here */
-
+    {
+      type:'input', 
+      name: 'title', 
+      message:'What is the title of your project?', 
+    },
+    {
+      type:'input', 
+      name: 'description', 
+      message:'Describe your project in two to three sentences.', 
+    },
+    {
+      type:'input', 
+      name: 'installation', 
+      message:'How do you install this project?', 
+    },
+    {
+      type:'input', 
+      name: 'usage', 
+      message:'How do you use this project?', 
+    },
+    {
+      type:'input', 
+      name: 'credits', 
+      message:'List any collaborators that you worked with and their github profile for this project. EX Sam: Samuel123', 
+    },
+    {
+      type:'list', 
+      name: 'license', 
+      message:'Choose a license for this project.', 
+      choices: ['MIT', 'BSD', 'GLP', 'none'], 
+    },
+    {
+      type:'input', 
+      name: 'contribution', 
+      message:'How would you like others to contribute to this project?', 
+    },
+    {
+      type:'input', 
+      name: 'tests', 
+      message:'Describe any tests that you have run on your project.', 
+    },
+    {
+      type:'input', 
+      name: 'questions', 
+      message:'How would you like others to reach you if they have questions?', 
+    },
+    {
+      type:'input', 
+      name: 'github', 
+      message:'What is your GitHub username?', 
+    },
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
+
   })
   .catch((error) => {
     if (error) {
